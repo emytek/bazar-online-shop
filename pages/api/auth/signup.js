66c67c30239bbd816594acc39a39843e41eb8528
emytek <66952283+emytek@users.noreply.github.com>
2023-12-25 15:01:09 +0100

    api_init middleware _auth on server

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,53 @@
+import { createRouter } from "next-connect";
+import db from "../../../utils/db";
+import { validateEmail } from "../../../utils/validation";
+import User from "../../../models/User";
+import bcrypt from "bcrypt";
+import { createActivationToken } from "../../../utils/tokens";
+// import { sendEmail } from "../../../utils/sendEmails";
+// import { activateEmailTemplate } from "../../../emails/activateEmailTemplate";
+
+const router = createRouter();
+
+router.post(async (req, res) => {
+  try {
+    await db.connectDb();
+    console.log(req.body);
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill in all fields." });
+    }
+    if (!validateEmail(email)) {
+      return res.status(400).json({ message: "Invalid email." });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "This email already exsits." });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters." });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: cryptedPassword });
+
+    const addedUser = await newUser.save();
+    //res.send(addedUser);
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
+    console.log(activation_token, "Token");
+    const url = `${process.env.BASE_URL}/activate/${activation_token}`;
+    res.send(url);
+    // sendEmail(email, url, "", "Activate your account.", activateEmailTemplate);
+    // await db.disconnectDb();
+    // res.json({
+    //   message: "Register success! Please activate your email to start.",
+    // });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default router.handler();
